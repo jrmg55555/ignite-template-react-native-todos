bc042ec81cb54dc3315fdb5ccbe5cf6d7ad55db7
Jean Mendes <jeanrodrigo30@gmail.com>
2021-11-04 21:55:18 -0400

    desafio 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 
 import { Header } from "../components/Header";
 import { Task, TasksList } from "../components/TasksList";
 import { TodoInput } from "../components/TodoInput";
 
+import { light, dark } from "../styles/themes";
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [darkTheme, setDarkTheme] = useState(false);
 
   function handleAddTask(newTaskTitle: string) {
+    const existTask = tasks.find((task) => task.title === newTaskTitle);
+
+    if (existTask) {
+      return Alert.alert(
+        "Task já cadastrada!",
+        "Você não pode cadastrar uma task com o mesmo nome."
+      );
+    }
+
     const newTask: Task = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -34,7 +46,35 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    setTasks(tasks.filter((task) => task.id !== id));
+    Alert.alert(
+      "Remover item",
+      "Você tem certeza que deseja remover este item?",
+      [
+        {
+          text: "Sim",
+          onPress: () => {
+            setTasks(tasks.filter((task) => task.id !== id));
+          },
+        },
+        {
+          text: "Não",
+          style: "cancel",
+        },
+      ]
+    );
+  }
+
+  function handleEditTask(taskId: number, taskNewTitle: string) {
+    const updatedTasks = tasks.map((task) => ({ ...task }));
+
+    const searchTask = updatedTasks.find((task) => task.id === taskId);
+
+    if (!searchTask) {
+      return;
+    }
+
+    searchTask.title = taskNewTitle;
+    setTasks(updatedTasks);
   }
 
   return (
@@ -47,6 +87,7 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   );
---
+++ src/styles/themes.tsx
@@ -0,0 +1,32 @@
+export const light = {
+  background: "#273FAD",
+  background_secondary: "#FFF",
+  primary: "#FFF",
+  secondary: "",
+  backgorund_input: "#F5F4F8",
+  shadowColor: "#000",
+  background_button: "#3FAD27",
+
+  taskHeaderColor: "#3D3D4D",
+  taskBorderColor: "#3D3D4D",
+  taskTextColor: "#3D3D4D",
+  taskButtonDone: "rgba(25, 61, 223, 0.1)",
+  taskTextDone: "#A09CB1",
+};
+
+export const dark = {
+  background: "#483C67",
+  background_secondary: "#1F1F1F",
+  primary: "#FFF",
+  secondary: "",
+  backgorund_input: "#34313D",
+  shadowColor: "#FF79C6",
+  background_button: "#988BC7",
+
+  taskHeaderColor: "#FF79C6",
+  taskBorderColor: "#FF79C6",
+  taskTextColor: "#FF79C6",
+  taskButtonDone: "rgba(255, 121, 198, 0.1)",
+  taskTextDone: "#E1E1E6",
+  backgroundTaksMark: "#FF79C6",
+};
